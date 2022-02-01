3386df50de4e522ff7aa645da87f796cd5a7efe5
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-01 10:07:45 +0300

    Test zmqpp linkage

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <zmqpp/zmqpp.hpp>
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    zmqpp::context context;
+    context.terminate();
     return 0;
 }
