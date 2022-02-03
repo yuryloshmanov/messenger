ccf66fc7049dfd40ae4eb723f6a7ef228c3b942a
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-03 19:41:35 +0300

    Add implementation

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "include/Server.hpp"
+
 
 int main() {
 
--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -2,9 +2,36 @@
 // Created by Yury Loshmanov on 02.02.2022.
 //
 
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 #include "../include/Server.hpp"
 
 
-Server Server::get() {
+Server &Server::get() {
     return instance;
+}
+
+
+void Server::configurePullSocketEndPoint(const std::string &endPoint) {
+    char buffer[256];
+    char *ipBuffer;
+    struct hostent *host_entry;
+
+    if (gethostname(buffer, sizeof buffer) == -1) {
+        throw std::runtime_error("Can't get hostname");
+    }
+
+    host_entry = gethostbyname(buffer);
+    if (!host_entry) {
+        throw std::runtime_error("Can't get host");
+    }
+
+    ipBuffer = inet_ntoa(*((struct in_addr *) host_entry->h_addr_list[0]));
+    if (!ipBuffer) {
+        throw std::runtime_error("Can't get ip");
+    }
+
+    std::string result(ipBuffer);
 }
