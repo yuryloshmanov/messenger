80aef643cab2ebd4e2a46481b520f7f11557e58a
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-03 19:40:58 +0300

    Add context, socket and config function

--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -5,13 +5,22 @@
 #ifndef SERVER_SERVER_HPP
 #define SERVER_SERVER_HPP
 
+#include <string>
+#include <zmqpp/zmqpp.hpp>
+
 
 class Server {
 public:
-    static Server get();
+    static Server &get();
+
+    static void configurePullSocketEndPoint(const std::string &endPoint);
 
 private:
+    zmqpp::context context{};
+    zmqpp::socket pullSocket{context, zmqpp::socket_type::pull};
+
     Server() = default;
+
     static Server instance;
 };
 
