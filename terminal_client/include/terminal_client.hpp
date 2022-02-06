da0fe452167ec35b105d700efa811496db4ed8e4
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-06 19:54:33 +0300

    Add template for class

--- terminal_client/include/terminal_client.hpp
+++ terminal_client/include/terminal_client.hpp
@@ -5,9 +5,26 @@
 #ifndef TERMINAL_CLIENT_TERMINAL_CLIENT_HPP
 #define TERMINAL_CLIENT_TERMINAL_CLIENT_HPP
 
+#include <string>
+#include <zmqpp/zmqpp.hpp>
+
 
 class TerminalClient {
+public:
+    // Default constructor
+    TerminalClient() = default;
 
+    // Connect to server via specified address
+    void connectToServer(const std::string &serverAdress);
+
+    // Main function to communicate with server
+    void run();
+
+private:
+    zmqpp::context context{};
+
+    // Socket to connect to server
+    zmqpp::socket pushSocket{context, zmqpp::socket_type::push};
 };
 
 
