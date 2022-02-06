1aa5fa3900b4dce2aba42629325d1f4259156714
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-06 20:01:09 +0300

    Add methods realization

--- terminal_client/src/terminal_client.cpp
+++ terminal_client/src/terminal_client.cpp
@@ -2,3 +2,18 @@
 // Created by Yury Loshmanov on 06.02.2022.
 //
 
+#include "../include/terminal_client.hpp"
+
+
+void TerminalClient::connectToServer(const std::string &serverAddress) {
+    pushSocket.connect(serverAddress);
+}
+
+
+void TerminalClient::run() {
+    zmqpp::message message;
+    std::string str = "Hello, ZeroMQ!";
+
+    message << str;
+    pushSocket.send(message);
+}
