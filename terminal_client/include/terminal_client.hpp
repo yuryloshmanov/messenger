fcb501c06ad0cbc78017f4d5e7106950614edd9f
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-06 19:33:39 +0300

    Add TerminalClinet files and class instance

---
+++ terminal_client/include/terminal_client.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Yury Loshmanov on 06.02.2022.
+//
+
+#ifndef TERMINAL_CLIENT_TERMINAL_CLIENT_HPP
+#define TERMINAL_CLIENT_TERMINAL_CLIENT_HPP
+
+
+class TerminalClient {
+
+};
+
+
+#endif //TERMINAL_CLIENT_TERMINAL_CLIENT_HPP
---
+++ terminal_client/src/terminal_client.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Yury Loshmanov on 06.02.2022.
+//
+
