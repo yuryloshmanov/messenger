b95776fff4e8ed5ff5935d90c2b917a1f6e1232d
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-15 22:32:50 +0300

    Update file header

--- server/include/Message/MessageType.hpp
+++ server/include/Message/MessageType.hpp
@@ -1,7 +1,8 @@
-//
-// Created by Yury Loshmanov on 13.02.2022.
-//
-
+/**
+ * @file MessageType.hpp
+ * @date 13 Feb 2022
+ * @author Yury Loshmanov
+ */
 #ifndef SERVER_MESSAGE_TYPE_HPP
 #define SERVER_MESSAGE_TYPE_HPP
 
