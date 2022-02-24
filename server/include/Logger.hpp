ddf5db9f73b12999566498a8e593bc625f65400e
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-24 23:24:17 +0300

    Change header

--- server/include/Logger.hpp
+++ server/include/Logger.hpp
@@ -1,7 +1,8 @@
-//
-// Created by Yury Loshmanov on 06.02.2022.
-//
-
+/**
+ * @file Logger.hpp
+ * @date 6 Feb 2022
+ * @author Yury Loshmanov
+ */
 #ifndef SERVER_LOGGER_HPP
 #define SERVER_LOGGER_HPP
 
