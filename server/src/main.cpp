3cd1e46a087b4f26c6cac2630f4ed6e94365c64a
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-25 23:00:04 +0300

    Set db proxy in main

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,11 +6,13 @@
 #include <iostream>
 
 #include "../include/Server.hpp"
+#include "../include/DatabaseProxy/PostgresqlProxy.hpp"
 
 
 int main() {
     try {
         Server::get().run();
+        Server::setDatabaseProxy(std::make_unique<PostgresqlProxy>(""));
     } catch (std::runtime_error &error) {
         std::cout << error.what() << std::endl;
 
