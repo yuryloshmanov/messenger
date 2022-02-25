2160c9844bbaf5910f68325d1e21e546c6335474
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-25 23:03:35 +0300

    Add todo

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -12,6 +12,7 @@
 int main() {
     try {
         Server::get().run();
+        // TODO: Move endPoint to config file
         Server::setDatabaseProxy(std::make_unique<PostgresqlProxy>(""));
     } catch (std::runtime_error &error) {
         std::cout << error.what() << std::endl;
