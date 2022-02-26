b2f00cd356f2e698363d991b651c24a36d2c5c22
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-26 21:37:27 +0300

    Add ConfigParser header file

---
+++ server/include/ConfigParser.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file ConfigParser.hpp
+ * @date 26 Feb 2022
+ * @author Yury Loshmanov
+ */
+#ifndef SERVER_CONFIGPARSER_HPP
+#define SERVER_CONFIGPARSER_HPP
+
+
+
+
+
+#endif //SERVER_CONFIGPARSER_HPP
