b48f6f034b055a143806d6e700dea11d49507548
Yury Loshmanov <yuryloshmanov@gmail.com>
2022-02-28 22:48:16 +0300

    Add PostgresqlURL test

--- server/test/test.cpp
+++ server/test/test.cpp
@@ -6,7 +6,20 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+#include "../include/ConfigParser.hpp"
 
-TEST(test, test) {
-    std::cout << "hello" << std::endl;
+
+TEST(ConfigParser, PostresqlURL) {
+    system(
+            "echo '{\n"
+            "  \"PostgresqlURL\": \"jdbc:postgresql://localhost:5432/postgres\"\n"
+            "}' > .config_parser_postgreqlurl"
+    );
+
+    ConfigParser configParser(".config_parser_postgreqlurl");
+    auto url = configParser.getPostgresqlURL();
+
+    system("rm -rf .config_parser_postgreqlurl");
+
+    ASSERT_EQ(url, "jdbc:postgresql://localhost:5432/postgres");
 }
